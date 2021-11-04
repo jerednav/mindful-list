f9967960234601184a1a7966756b3fcd48f14e18
jerednav <80506833+jerednav@users.noreply.github.com>
2021-11-04 07:27:24 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route } from 'react-router-dom'
+//Route defines different paths in the URL, and which component should be loaded
+import Home from './pages/Home'
+import NewTask from './pages/NewTask'
+import Search from './pages/Search'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route path ='/'>
+        <Home />
+      </Route>
+      <Route path ='/newtask'>
+        <NewTask />
+      </Route>
+      <Route path ='/search'>
+        <Search />
+      </Route>
+    
     </div>
   );
 }
