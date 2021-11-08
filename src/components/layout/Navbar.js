ca9e107a5985f771f7cba7c93226f77c8b334604
jerednav <80506833+jerednav@users.noreply.github.com>
2021-11-08 00:40:59 -0700

    Added Navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,10 +1,17 @@
-import React from "react";
+import "./Navbar.css";
 
 const Header = () => {
   return (
     <div className='navContainer'>
       <div class='navWrapper'>
-        <div class='logo'>mindful {`<List />`} </div>
+        <div className='logo'>mindful {`<List />`} </div>
+        <div className='menu'>
+          <ul className='navList'>
+            <li className='navSection'>Home</li>
+            <li className='navSection'>New Task</li>
+            <li className='navSection'>Search</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
