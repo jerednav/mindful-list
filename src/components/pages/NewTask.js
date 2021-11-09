1e813e2d5d9885043799a195c254370b958378eb
jerednav <80506833+jerednav@users.noreply.github.com>
2021-11-09 10:00:01 -0700

    Add logo and task input to main page

--- src/components/pages/NewTask.js
+++ src/components/pages/NewTask.js
@@ -1,8 +1,8 @@
-import React from "react";
+import "./NewTask.css";
 
 function NewTask() {
   return (
-    <form>
+    <form className='newTaskContainer'>
       <input type='text' placeholder='Enter task' />
     </form>
   );
