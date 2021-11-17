ae63aca1e57a1edfbef064b2dfcd99a53f8a7050
jerednav <80506833+jerednav@users.noreply.github.com>
2021-11-16 21:12:30 -0800

    Finished importing Redux

--- client /src/index.js
+++ client /src/index.js
@@ -3,9 +3,18 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
+import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 
+import rootReducer from "./store/reducers/rootReducer";
+
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+<Provider store ={ store }>
+    <App /> 
+</Provider>,
+
+document.getElementById("root")
+);
