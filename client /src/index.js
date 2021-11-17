a095ee3731c002b0ebd586582ab806f30921086f
jerednav <80506833+jerednav@users.noreply.github.com>
2021-11-16 20:54:20 -0800

    Addd redux

--- client /src/index.js
+++ client /src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
 ReactDOM.render(<App />, document.getElementById("root"));
